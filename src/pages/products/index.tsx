70e3d29c2b570d0ab0cd098cc026c193035231d3
Gabriel Lima <gabrielcontato06@gmail.com>
2026-02-16 14:30:25 -0300

    Primeira estrutura da página de produtos

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -1,7 +1,33 @@
-import {Title} from './styled-components'
+import * as S from './styled-components'
+
+const optionsFilter = [
+    { value: 'categoria1', label: 'Categoria 1' },
+    { value: 'categoria2', label: 'Categoria 2' },
+    { value: 'categoria3', label: 'Categoria 3' },
+];
 
 export default function Products() {
     return(
-       <Title>Página de produtos</Title>
+        <S.ContentInfo>
+            <S.Title>Product Page</S.Title>
+            <S.Text>Manage your products in the system.</S.Text>
+
+            <S.ContentHandler>
+                <S.AddProductBtn>
+                    <S.IconAddBtn/>
+                    Add Product
+                </S.AddProductBtn>
+                <S.FilterSelect name='filter' value=''>
+                    <S.FilterOption className='optionStyle' value='' disabled>
+                        Filter
+                    </S.FilterOption>
+                    {optionsFilter.map((item) => (
+                        <S.FilterOption key={item.value} value={item.value}>
+                            {item.label}
+                        </S.FilterOption>
+                    ))}
+                </S.FilterSelect>
+            </S.ContentHandler>
+        </S.ContentInfo>
     )
 }
--- src/pages/products/styled-components.ts
+++ src/pages/products/styled-components.ts
@@ -1,9 +1,103 @@
 import styled from 'styled-components'
 
+import { FaPlus } from "react-icons/fa";
+
 const Title = styled.h1`
-    font-size: var(--text-lg);
+    font-size: var(--text-xl);
+    margin: 0 0 var(--space-sm) 0;
 `
 
+const Text = styled.p`
+    font-size: var(--text-md);
+    font-weight: var(--text-w-bold);
+    color: var(--color-text-secondary);
+    margin: 0 0 var(--space-md) 0;
+
+`
+
+const ContentInfo = styled.div`
+    display: flex;
+    flex-direction: column;
+    padding: var(--space-xxl);
+    gap: var(--space-md);
+    margin-top: var(--space-lg);
+`
+
+const ContentHandler = styled.div`
+    display: flex;
+    justify-content: flex-end;
+    gap: var(--space-md);
+    border: solid;
+    border-width: var( --space-xxs) 0 var( --space-xxs) 0;
+    border-color: var( --color-border);
+    padding: var(--space-md);
+`
+
+const AddProductBtn = styled.button`
+    width: 165px;
+    height: 45px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 8px;
+    background-color: var( --color-primary);
+    color: var( --color-text-primary);
+    font-size: var(--text-md);
+    font-weight: var(--text-w-bold);
+    border-radius: var(--radius-md);
+
+    &:hover{
+        background-color: var( --color-primary-hover);
+        color: var( --color-text-primary);
+    }
+`
+
+const IconAddBtn = styled(FaPlus)`
+    color: var( --color-text-primary);
+    font-size: 16px;
+    font-weight: var(--text-w-bold);
+`
+
+const FilterSelect = styled.select`
+    width: 215px;
+    height: 45px;
+    border-radius: var(--radius-md);
+    font-size: var(--text-md);
+    font-weight: var(--text-w-bold);
+    color: var(--color-text-secondary);
+    background-color: var( --color-card);
+    border: 1px solid var(--color-border);
+
+    padding: 0 var(--space-md);
+
+    cursor: pointer;
+
+    &:focus {
+        outline: none;
+        border-color: var(--color-primary);
+    }
+`
+const FilterOption = styled.option`
+    background-color: var( --color-card);
+    color: var(--color-text-secondary);
+    font-weight: var(--text-w-bold);
+
+    &:hover{
+        background-color: var(--color-bg-light) !important;
+        color: var( --color-text-hover);
+    }
+`
+
+
+
+
 export{
-    Title
+    Title,
+    Text,
+    ContentInfo,
+    ContentHandler,
+    AddProductBtn,
+    IconAddBtn,
+    FilterSelect,
+    FilterOption,
 }
